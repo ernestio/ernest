a4c88816acbccaabb1706e3062b26247916304c0
Mark Newman <mark.newman@r3labs.io>
2017-09-13 13:52:14 +0100

    Fix GOPATH

--- internal/ci_install_service.sh
+++ internal/ci_install_service.sh
@@ -8,9 +8,9 @@ echo $ORG
 echo $REPO
 echo $BRANCH
 
-mkdir -p /home/ubuntu/.go_workspace/src/github.com/$ORG/
-cd /home/ubuntu/.go_workspace/src/github.com/$ORG/ && rm -rf $REPO && git clone git@github.com:$ORG/$REPO.git
-cd /home/ubuntu/.go_workspace/src/github.com/$ORG/$REPO/
+mkdir -p $GOPATH/src/github.com/$ORG/
+cd $GOPATH/src/github.com/$ORG/ && rm -rf $REPO && git clone git@github.com:$ORG/$REPO.git
+cd $GOPATH/src/github.com/$ORG/$REPO/
 if [ -n "$BRANCH" ];
   then git checkout $BRANCH
 fi
